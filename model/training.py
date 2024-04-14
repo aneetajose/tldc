525f9c89549617b43e471e11d356f41a7b81e5c6
Aneeta <itsaneetajose@gmail.com>
2024-04-14 23:08:09 +0530

    Updated training.py

--- model/training.py
+++ model/training.py
@@ -1,17 +1,38 @@
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-img_dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    'Dataset',
-    shuffle=True,
-    image_size=(256, 256),
-    batch_size=32)
-class_names = img_dataset.class_names
-print(class_names)
-print(len(img_dataset))
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.preprocessing.image import ImageDataGenerator
+
+# Define the directory where your dataset is stored
+data_directory = '../Dataset/train_data'
+
+# Define image data generator for preprocessing and augmentation
+datagen = ImageDataGenerator(
+    rescale=1./255,  # Normalize pixel values to [0, 1]
+    validation_split=0.2  # Percentage of data to use for validation
+)
+
+# Load and split the dataset into training and testing sets
+train_generator = datagen.flow_from_directory(
+    data_directory,
+    target_size=(256, 256),
+    batch_size=32,
+    class_mode='sparse',
+    subset='training',
+    seed=42
+)
+
+validation_generator = datagen.flow_from_directory(
+    data_directory,
+    target_size=(256, 256),
+    batch_size=32,
+    class_mode='sparse',
+    subset='validation',
+    seed=42
+)
+
 # Define model architecture
 model = Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+    Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3)),
     MaxPooling2D((2, 2)),
     Conv2D(64, (3, 3), activation='relu'),
     MaxPooling2D((2, 2)),
@@ -28,8 +49,7 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 # Train the model
-model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+model.fit(train_generator, epochs=10, validation_data=validation_generator)
 
-# Evaluate the model
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print('Test accuracy:', test_acc)
+# Save the model
+model.save('classifier.keras')
